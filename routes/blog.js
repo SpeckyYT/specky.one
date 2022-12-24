846fc42daf43952fb6bc6406ad75ba20cccb78a9
SpeckyYT <Specky.fans@gmail.com>
2022-12-24 15:24:15 +0100

    updated blog embeds

--- routes/blog.js
+++ routes/blog.js
@@ -18,9 +18,28 @@ const blogs = [
             'Specky',
         ],
         tags: ["mancala", "game", "computation", "math"],
+        embed: {
+            title: "How many unique games of Mancala are there?",
+            description: "In this blog, we'll explore how many distinct games of Mancala there are.",
+            url: "mancala", // url is defaulted to blog.path
+            // if you need a specific url, just input "hello" and it will result to "/blog/hello"
+            image: "https://acrosstheboardgame.com/wp-content/uploads/2018/07/Mancala-at-play-angled.jpg",
+            color: "#C19A6B",
+        },
     },
 ]
-.filter(blog => fs.existsSync(path.join(process.cwd(), "views", "blog", blog.filename)));
+.filter(blog => fs.existsSync(path.join(process.cwd(), "views", "blog", blog.filename)))
+.map(blog => {
+    blog.embed ??= {}
+
+    blog.embed.title ??= blog.title
+    blog.embed.description ??= 'Here\'s a new ""discovery"" Specky made'
+    blog.embed.url = `https://specky.one/blog/${blog.embed.url || blog.path}`
+    blog.embed.image ??= "https://upload.wikimedia.org/wikipedia/commons/4/42/Blog_%281%29.jpg"
+    blog.embed.color ??= "#2A10BA"
+
+    return blog
+});
 
 let tags = new Set();
 for(const blog of blogs) {
