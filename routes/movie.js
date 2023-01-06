31357ed1e68a740f263ddd28a64aa2ee654d75c5
SpeckyYT <Specky.fans@gmail.com>
2023-01-06 22:36:06 +0100

    even more movie progress

--- routes/movie.js
+++ routes/movie.js
@@ -1,19 +1,85 @@
+// https://developers.themoviedb.org/3/
+
 const { Router } = require('express');
 const router = Router();
 const { default: axios } = require('axios');
+const colors = require('colors/safe');
 
-const cache = {}
+const movies = require('./movie/movies.coffee');
+const log = require('../util/log.js');
+
+const movieDataCache = {}
+const movieImageCache = {}
+
+const defaultImage = "https://as1.ftcdn.net/v2/jpg/02/43/62/78/1000_F_243627831_mZP0TwkljMspNOW8zxMsSvWJhLi749ig.jpg"
 
 if(!TMDB_KEY) throw "TMDB_KEY in .env missing"
 
-router.get("/", (req, res) => {
-    res.render("movie/main.pug", { req, res })
+const wait = ms => new Promise(res => setTimeout(res, ms));
+
+(async function() {
+    for(const movie of movies) {
+        await wait(5000);
+        const startTime = Date.now();
+        try {
+            await getMovieData(movie.id);
+            if(DEBUG) {
+                log(`Movie #${movie.id}`, "cached", colors.magenta, startTime);
+            }
+        } catch(err) {
+            if(DEBUG) {
+                log(`Movie #${movie.id}`, `${err}`, colors.red, startTime, true);
+            }
+        }
+    }
+})();
+
+router.get("/", async (req, res) => {
+    try {
+        res.render("movie/main.pug", {
+            req,
+            res,
+            movies: (await Promise.all(
+                movies.map(
+                    async m => await getMovieData(m.id)
+                    .catch(()=>{})
+                )
+            ))
+            .filter(f=>f),
+        })
+    } catch(err) {
+        res.render("error.pug", { req, res, error: `${err}` })
+    }
 })
 
 router.get("/:movie", async (req, res) => {
     const movieID = req.params.movie;
-    if(cache[movieID]) {
-        return res.json(cache[movieID]);
+
+    try {
+        const data = await getMovieData(movieID);
+
+        return res.json(data);
+    } catch(err) {
+        if(Array.isArray(err)) {
+            return res.status(err[0]).send(...[err[1]].filter(v => v));
+        }
+    }
+})
+
+router.get("/image/:image", (req, res) => res.send(originalImage(req.params.image)))
+
+function generateMovieLink(movieID = 1, apiVersion = 3) {
+    return `https://api.themoviedb.org/${apiVersion}/movie/${movieID}?api_key=${TMDB_KEY}`
+}
+
+async function getMovieData(movieID) {
+    movieID = `${movieID}`;
+
+    if(movieDataCache[movieID]) {
+        if(Array.isArray(movieDataCache[movieID])) {
+            throw movieDataCache[movieID];
+        }
+        return movieDataCache[movieID];
     }
 
     const linkV3 = generateMovieLink(movieID, 3);
@@ -25,19 +91,51 @@ router.get("/:movie", async (req, res) => {
 
         const output = { ...outputV3.data, ...outputV4.data }; 
 
-        cache[movieID] = output;
-
-        return res.json(output);
-    } catch (err) {
-        if(err?.response?.status == 404) {
-            return res.status(404).send() 
+        // IMAGES
+        await Promise.all(
+            ["backdrop_path", "poster_path"]
+            .map(async i => {
+                if(output[i]) {
+                    output[i] = await originalImage(output[i]);
+                }
+            })
+        );
+        if(Array.isArray(output.production_companies)) {
+            output.production_companies = await Promise.all(
+                output.production_companies.map(async comp => {
+                    const o = comp;
+                    if(o.logo_path) o.logo_path = await originalImage(o.logo_path);
+                    return o;
+                })
+            )
         }
-        return res.status(400).send(`${err}`)
-    }
-})
 
-function generateMovieLink(movieID = 1, apiVersion = 3) {
-    return `https://api.themoviedb.org/${apiVersion}/movie/${movieID}?api_key=${TMDB_KEY}`
+        // APPEND SPECKY.ONE DATA
+        output.speckyone = movies.find(m => `${m.id}` == movieID) || null;
+
+        movieDataCache[movieID] = output;
+
+        return output;
+    } catch (err) {
+        movieDataCache[movieID] = [err?.response?.status || 400, `${err}`];
+        throw movieDataCache[movieID]
+    }
+}
+
+async function originalImage(imageLink) {
+    if(movieImageCache[imageLink]) return movieImageCache[imageLink];
+
+    const link = `https://image.tmdb.org/t/p/original/${imageLink}`;
+
+    try {
+        const image = await axios(link);
+        if(image.status >= 400) throw "";
+        movieImageCache[imageLink] = link;
+        return link;
+    } catch(err) {
+        movieImageCache[imageLink] = defaultImage;
+        return defaultImage;
+    }
 }
 
 module.exports = {
---
+++ routes/movie/movies.coffee
@@ -0,0 +1,14 @@
+# TODO: THESE ARE CURRENTLY TEMPORARY AND ONLY USED FOR TESTING
+
+all_movies = [
+    id: 551
+    rate: 8/10
+    opinion: "what the fuck is this movie"
+,
+    id: 11
+    rate: 5/10
+    opinion: "idk never watched this movie"
+]
+.sort((a,b) => a.id - b.id)
+
+module.exports = all_movies
